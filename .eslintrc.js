93199276aca0e1c041ea3d6481cc7a422d506886
ZeVictor <jose.victor_dantas@hotmail.com>
2021-08-18 18:13:29 -0300

    chore: set eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['src/components/organisms/TradingView/'],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -23,20 +19,12 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    '@typescript-eslint',
     'react',
     'simple-import-sort',
     'sort-keys-custom-order-fix',
     'unused-imports'
   ],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
     'no-useless-escape': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
@@ -47,29 +35,6 @@ module.exports = {
       'custom',
       {
         caseSensitive: true,
-        order: [
-          '_id',
-          'id',
-          'userId',
-          'username',
-          'followerUserId',
-          'followedUserId',
-          'exchangeId',
-          'symbolId',
-          'method',
-          'headers',
-          'base',
-          'sm',
-          'md',
-          'lg',
-          'xl',
-          '2xl',
-          'localhost',
-          'stg',
-          'prd',
-          'staging',
-          'production'
-        ],
         orderBy: 'asc'
       }
     ],
